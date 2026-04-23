37d004d86970049dd3d3ab3da3598087f9fc7ec4
FGCat-coder <felixgash20@gmail.com>
2026-04-23 21:00:26 +0900

    Create conditioner.py
    Добавлен пример с логическим оператором and, который в данном случае включает кондиционер

---
+++ conditioner.py
@@ -0,0 +1,6 @@
+temp = 21
+presence = 1
+conditioner = (temp > 20) and presence > 0
+print(conditioner)
+
+# Добавлен пример с логическим оператором and, который в данном случае включает кондиционер
